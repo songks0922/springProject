057ac5dbc9658e20cf77abdbd95518781dbbcf15
Song kyeong seok <skj0922@kookmin.ac.kr>
2022-02-12 17:47:07 +0900

    4장 4.4 DTO와 서비스 계층 추가

---
+++ guestbook/src/main/java/org/zerock/guestbook/service/GuestbookServiceImpl.java
@@ -0,0 +1,11 @@
+package org.zerock.guestbook.service;
+
+import org.zerock.guestbook.dto.GuestbookDTO;
+
+public class GuestbookServiceImpl implements GuestbookService{
+
+    @Override
+    public Long register(GuestbookDTO dto) {
+        return null;
+    }
+}
